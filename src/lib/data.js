590ffe6bef903d66930702697eb760391e6498fb
Craig Clayton <craig__clayton@outlook.com>
2025-01-22 14:46:22 +0000

    make lib folder with data file for images

---
+++ src/lib/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    imageUrl:
+      'https://images.unsplash.com/photo-1521309569781-7bcd429eb2f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZW5nbGlzaCUyMGJ1bGxkb2d8ZW58MHx8MHx8fDA%3D',
+    alt: 'image of English Bulldog',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://images.unsplash.com/photo-1648047404073-a771e51b7851?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGVuZ2xpc2glMjBidWxsZG9nfGVufDB8fDB8fHww',
+    alt: 'image of English Bulldog',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://plus.unsplash.com/premium_photo-1664303056344-e900d56faa49?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZW5nbGlzaCUyMGJ1bGxkb2d8ZW58MHx8MHx8fDA%3D',
+    alt: 'image of English Bulldog',
+  },
+  {
+    id: 4,
+    imageUrl:
+      'https://plus.unsplash.com/premium_photo-1723709063189-a00e99de226a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZW5nbGlzaCUyMGJ1bGxkb2d8ZW58MHx8MHx8fDA%3D',
+    alt: 'image of English Bulldog',
+  },
+  {
+    id: 5,
+    imageUrl:
+      'https://images.unsplash.com/photo-1728930973803-64502b03dd3d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGVuZ2xpc2glMjBidWxsZG9nfGVufDB8fDB8fHww',
+    alt: 'image of English Bulldog',
+  },
+];
